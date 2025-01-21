dd7baf24bbf4788956a9a2d498a283e330b1b968
Jacqueline Braga <jaquelinesantosbraga@gmail.com>
2025-01-21 11:46:44 -0300

    .

---
+++ Cap01/Exercicios/HackerRank/Arithmetic-Operators.py
@@ -0,0 +1,13 @@
+sum = lambda a, b: a + b
+
+difference = lambda a, b: a - b
+
+product = lambda a, b: a * b
+
+if __name__ == '__main__':
+      a = int(input())
+      b = int(input())
+
+      print(sum(a, b))
+      print(difference(a, b))
+      print(product(a, b))
---
+++ Cap01/Exercicios/HackerRank/Division.py
@@ -0,0 +1,12 @@
+int_division = lambda a, b: a // b
+
+float_division = lambda a, b: a / b
+
+if __name__ == '__main__':
+      a = int(input())
+      b = int(input())
+
+      print(int_division(a, b))
+      print(float_division(a, b))
+
+
